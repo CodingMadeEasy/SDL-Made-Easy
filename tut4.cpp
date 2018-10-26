555c014c2e71cc8388a8073fd837e57d8916d9cd
Brain Developer <ssaulakh2@gmail.com>
2018-10-26 14:39:00 +0530

    Update tut4.cpp
    tut4 commented out.

--- tut4.cpp
+++ tut4.cpp
@@ -1,28 +1,28 @@
-#include<SDL.h>
-#include<iostream>
+#include<SDL.h> // The main Header file of SDL library
+#include<iostream> // input output stream header file
 
 int main(int argc, char *argv[])
 {
-	SDL_Window *window = nullptr;
-	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window *window = nullptr; // Allocating space for Display Window.
+	SDL_Init(SDL_INIT_VIDEO); // Initializing SDL
 	
-	window = SDL_CreateWindow("SDL CodingMadeEasy Series", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow("SDL CodingMadeEasy Series", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN); // Creating Display Window with following attributes
 
-	bool isRunning = true; 
-	SDL_Event ev;
+	bool isRunning = true; // Game loop triger
+	SDL_Event ev; // Allocating space for Event ev
 
-	while(isRunning)
+	while(isRunning) // main Game loop
 	{
-		while(SDL_PollEvent(&ev) != 0)
+		while(SDL_PollEvent(&ev) != 0) // main Event loop, checks if Event ev is empty
 		{
-			if(ev.type == SDL_QUIT)
-				isRunning = false;
+			if(ev.type == SDL_QUIT) // if windows is closed
+				isRunning = false; // Game loop triger set to false
 		}
-		SDL_UpdateWindowSurface(window);
+		SDL_UpdateWindowSurface(window); //Redrawing everything on the Display Window
 	}
 
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
+	SDL_DestroyWindow(window); // Deallocating resources acquired by Display window.
+	SDL_Quit(); // Deallocating the Initialized SDL.
+	
 	return 0;
 }
